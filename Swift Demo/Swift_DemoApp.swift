b7c8037b413bbae07793bfc80e05d542fb216bf9
stevensJourney <steven@journeyapps.com>
2025-09-04 15:17:49 -0600

    initial xcode project

---
+++ Swift Demo/Swift_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DemoApp.swift
+//  Swift Demo
+//
+//  Created by Steven Ontong on 03-09-2025.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift DemoTests/Swift_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_DemoTests.swift
+//  Swift DemoTests
+//
+//  Created by Steven Ontong on 03-09-2025.
+//
+
+import Testing
+
+struct Swift_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
